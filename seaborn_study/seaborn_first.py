928d2cd32049904a594958d77fe3dfd31966c5a3
regex-young <regex.young@gmail.com>
2018-07-19 16:57:37 +0800

    Update seaborn_first.py

--- seaborn_study/seaborn_first.py
+++ seaborn_study/seaborn_first.py
@@ -1,3 +1,21 @@
 #! /bin/env python
 
 print('hello seaborn')
+
+import pandas as pd
+import numpy as np
+
+from matplotlib import pyplot as plt
+
+import seaborn as sns
+
+df = pd.read_csv('Pokemon.csv',index_col=0)
+
+df.head()
+
+#sns.lmplot(x = 'Attack', y = 'Defense', data = df)
+
+
+sns.set_style("whitegrid")
+plt.plot(np.arange(10))
+plt.show()
